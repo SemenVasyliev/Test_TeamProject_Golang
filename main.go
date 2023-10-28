0f335aa5cd21ebad6fd8cc419d2f9ce2e0bbb60e
felixpincher <felixpincher@gmail.com>
2023-10-29 00:27:31 +0300

    added editor page

--- main.go
+++ main.go
@@ -5,12 +5,32 @@ import (
 	"net/http"
 )
 
+type User struct {
+	id       uint
+	login    string
+	password string
+	name     string
+	age      uint16
+}
+
 func main() {
+	handleRequest()
+}
+
+func handleRequest() {
 	// call a home page and start of func "home_page"
 	http.HandleFunc("/", home_page)
+	http.HandleFunc("/editor-page/", editor_page)
 	// Added a local server with parametrs (nil = null = none)
 	http.ListenAndServe(":8080", nil)
 }
-func home_page(page http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(page, "Go is super easy!")
+
+func editor_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Editor page")
+}
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	bob := User{1, "ddd", "222", "BOB", 1}
+	bob.name = "Alex"
+	fmt.Fprintf(w, "User name is: "+bob.name)
 }
