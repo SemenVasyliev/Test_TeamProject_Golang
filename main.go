21c062c9cf5fa4c24c0c0c5caffd07bff82068d4
felixpincher <felixpincher@gmail.com>
2023-10-30 03:56:53 +0100

    added an html file and styles

--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type User struct {
 	id       uint
-	login    string
-	password string
-	name     string
-	age      uint16
+	Login    string
+	Password string
+	Name     string
+	Age      uint16
+	Articles []string
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d."+
+		"And his login is %s", u.Name, u.Age, u.Login)
+}
+
+// * - using an object like a link (using a link on the object)
+func (u *User) setNewName(newName string) {
+	u.Name = newName
 }
 
 func main() {
@@ -18,7 +30,7 @@ func main() {
 }
 
 func handleRequest() {
-	// call a home page and start of func "home_page"
+	// call a homeа page and start of func "home_page"
 	http.HandleFunc("/", home_page)
 	http.HandleFunc("/editor-page/", editor_page)
 	// Added a local server with parametrs (nil = null = none)
@@ -30,7 +42,9 @@ func editor_page(w http.ResponseWriter, r *http.Request) {
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-	bob := User{1, "ddd", "222", "BOB", 1}
-	bob.name = "Alex"
-	fmt.Fprintf(w, "User name is: "+bob.name)
+	bob := User{1, "ddd", "222", "BOB", 1, []string{"A1", "A2", "A3"}}
+	// bob.setNewName("Alex")
+	// fmt.Fprintf(w, bob.getAllInfo())
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
 }
